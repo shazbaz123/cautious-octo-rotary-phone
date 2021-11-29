ad0ededf77d7dc9cebd66c16aec312a75ab62d7f
shazbaz123 <shaya.barron@gmail.com>
2021-11-29 14:06:51 +0000

    new files

---
+++ src/utils/index.js
@@ -0,0 +1,15 @@
+const movieArr = [];
+
+class Movie {
+    constructor(title, actor, director) {
+        this.title = title;
+        this.actor = actor;
+        this.director = director;
+    }
+
+    add() {
+        movieArr.push(this);
+    }
+}
+
+module.exports = {Movie, movieArr}
